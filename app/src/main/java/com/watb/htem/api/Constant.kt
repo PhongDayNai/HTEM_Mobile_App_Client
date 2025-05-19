693fc3dc52a6a39498f6dd503ae605ac9b46d023
Dương Phong <dhphong266@gmail.com>
2025-05-19 18:35:22 +0700

    add new server url

--- app/src/main/java/com/watb/htem/api/Constant.kt
+++ app/src/main/java/com/watb/htem/api/Constant.kt
@@ -13,10 +13,10 @@ class Constant {
             get() = Main_Url + "api/users/change-password"
         val User_Points_Url: String
             get() = Main_Url + "api/users/points"
-        //        val User_Points_History_Url: String
-//            get() = Main_Url + "api/users/points-history" // Server thieu
         val User_Transaction_Url: String
-            get() = Main_Url + "api/users/get-transaction" // Server thieu
+            get() = Main_Url + "api/users/get-transaction"
+        val Call_Staff_Url: String
+            get() = Main_Url + "api/users/call-staff"
 
         val Guess_Buffet_Url: String
             get() = Main_Url + "api/buffets/guess"
@@ -25,16 +25,18 @@ class Constant {
 
         val Add_Dishes_Url: String
             get() = Main_Url + "api/orders/add-dishes"
-        val Served_Dishes_Url: String
-            get() = Main_Url + "api/orders/served"
+        val Get_Served_Dishes_Url: String
+            get() = Main_Url + "api/orders/get-served"
 
         val Payment_User_Url: String
             get() = Main_Url + "api/payments/handleUser"
         val Payment_Guess_Url: String
             get() = Main_Url + "api/payments/handleGuess"
+        val Check_Payment_State_Url: String
+            get() = Main_Url + "api/payments/state"
 
         fun setMainUrl(url: String) {
-            Main_Url = "https://${url}.loca.lt/" // Phai co / o cuoi
+            Main_Url = "https://${url.trim()}.loca.lt/" // Phai co / o cuoi
         }
     }
 }
